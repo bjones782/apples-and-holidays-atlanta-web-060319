abce346ee3d62d1a10df406272f9fbc96d8fcdb1
Brooke Jones <bjones782@gmail.com>
2019-05-16 19:46:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,7 +21,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  
+  holiday_supplies[:summer][:fourth_of_july][1]
+end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
